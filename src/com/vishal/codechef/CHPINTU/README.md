b8b15d70e757a43af0eb3cede5fd7ebe2be8d65e
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-27 18:25:02 +0530

    CHPINTU

---
+++ src/com/vishal/codechef/CHPINTU/README.md
@@ -0,0 +1,7 @@
+# Question Code : CHPINTU
+
+Chef went to Australia and saw the destruction caused by bushfires, which made him sad, so he decided to help the animals by feeding them fruits. First, he went to purchase fruits from Pintu.
+
+Pintu sells M different types of fruits (numbered 1 through M). He sells them in N baskets (numbered 1 through N), where for each valid i, the i-th basket costs Pi and it contains fruits of type Fi. Chef does not have too much money, so he cannot afford to buy everything; instead, he wants to choose one of the M available types and purchase all the baskets containing fruits of that type.
+
+Help Chef choose the type of fruit to buy such that he buys at least one basket and the total cost of the baskets he buys is the smallest possible.
---
+++ src/com/vishal/codechef/CHPINTU/Solution.java
@@ -0,0 +1,40 @@
+package com.vishal.codechef.CHPINTU;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i =0;i<t;i++){
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            int[] a= new int[n];
+            int[] b= new int[n];
+            for(int j=0;j<n;j++){
+                a[j] = sc.nextInt();
+            }
+            for(int j=0;j<n;j++){
+                b[j] = sc.nextInt();
+            }
+            int sum = 10000;
+            for(int j=1 ; j<=m;j++){
+                boolean chk =false;
+                int ans = 0;
+                for(int k=0;k<n;k++){
+
+                    if(a[k]==j){
+                        ans+=b[k];
+                        chk =true;
+                    }
+                }
+                if(chk){
+                    sum = Math.min(sum,ans);
+                }
+            }
+
+            System.out.println(sum);
+        }
+    }
+}
