e8262bccdca15416a0bd6a47bef2d2172aa17de2
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-28 16:39:04 +0530

    find digits

---
+++ src/com/vishal/hackerrank/FindDigits/Solution.java
@@ -0,0 +1,58 @@
+package com.vishal.hackerrank.FindDigits;
+
+import java.io.*;
+import java.util.stream.IntStream;
+
+class Result {
+
+    /*
+     * Complete the 'findDigits' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int findDigits(int n) {
+        // Write your code here
+        int count=0,temp=n,o;
+
+        while(temp>0)
+        {
+            o=temp%10;
+            temp/=10;
+            if(o!=0)
+            {
+                if(n%o==0)
+                    count++;
+            }
+        }
+
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int result = Result.findDigits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
