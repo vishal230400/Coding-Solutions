053c2afb3efa612dd1f4681627a3b84480018a57
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-28 16:11:34 +0530

    Compare the Triplets

---
+++ src/com/vishal/hackerrank/ComparetheTriplets/Solution.java
@@ -0,0 +1,67 @@
+package com.vishal.hackerrank.ComparetheTriplets;
+
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<Integer> compareTriplets(List<Integer> a, List<Integer> b) {
+        // Write your code here
+        List<Integer> count=new ArrayList<>();
+        int acount=0,bcount=0;
+
+        for(int i =0;i<a.size();i++)
+        {
+            if(a.get(i)>b.get(i))
+                acount++;
+            if(a.get(i)<b.get(i))
+                bcount++;
+
+        }
+
+        count.add(acount);
+        count.add(bcount);
+
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> b = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.compareTriplets(a, b);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
