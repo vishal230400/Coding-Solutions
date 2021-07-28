88fd4306dcf48d1e502b22ad8e352dfbd7e2b27b
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-28 09:10:17 +0530

    CMAX02

--- src/com/vishal/codechef/CMAX02/Solution.java
+++ src/com/vishal/codechef/CMAX02/Solution.java
@@ -12,28 +12,24 @@ public class Solution {
             int n=sc.nextInt();
             int div=n-1;
             ArrayList<Integer> rem = new ArrayList<>();
-            int temp =1;
+            int temp;
             while(div!=0)
             {
                 temp=div%3;
                 div=div/3;
                 if(temp==0) {div--;}
-                if(temp==0 && div==0) {temp=1;}
                 rem.add(temp);
             }
-            if(temp!=0)
-            {
-                rem.add(0);
-            }
+            rem.add(0);
             StringBuilder rem0=new StringBuilder();
             StringBuilder rem1=new StringBuilder();
             for(int i=0;i<rem.size();i++)
             {
                 if(rem.get(i).equals(0))
-                    rem0.append(i).append(" ");
+                    rem0.append(i+" ");
                 else if(rem.get(i).equals(1))
                 {
-                    rem1.append(i).append(" ");
+                    rem1.append(i+" ");
                 }
             }
             System.out.println(rem0);
