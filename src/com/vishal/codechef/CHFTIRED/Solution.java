01127a01ff6c2d29d4ffb41a3a83b3081c631003
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-28 11:58:36 +0530

    CHFTIRED

---
+++ src/com/vishal/codechef/CHFTIRED/Solution.java
@@ -0,0 +1,19 @@
+package com.vishal.codechef.CHFTIRED;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int a;
+            a=sc.nextInt();
+            a=sc.nextInt();
+            System.out.println("YES");
+        }
+        sc.close();
+   }
+}
