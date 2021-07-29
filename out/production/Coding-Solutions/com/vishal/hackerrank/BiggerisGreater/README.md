3224977b14116215f777d9bbc0a7c3f53e87b6f3
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-29 09:26:49 +0530

    climb

---
+++ out/production/Coding-Solutions/com/vishal/hackerrank/BiggerisGreater/README.md
@@ -0,0 +1,8 @@
+# Question Name : BiggerisGreater
+
+Lexicographical order is often known as alphabetical order when dealing with strings. A string is greater than another string if it comes later in a lexicographically sorted list.
+
+Given a word, create a new word by swapping some or all of its characters. This new word must meet two criteria:
+
+It must be greater than the original word
+It must be the smallest word that meets the first condition
---
+++ src/com/vishal/hackerrank/ClimbingtheLeaderboard/Solution.java
@@ -0,0 +1,86 @@
+package com.vishal.hackerrank.ClimbingtheLeaderboard;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'climbingLeaderboard' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY ranked
+     *  2. INTEGER_ARRAY player
+     */
+
+    public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
+        // Write your code here
+        Set<Integer> set = new LinkedHashSet<>(ranked);
+        ranked.clear();
+        ranked.addAll(set);
+        int num = ranked.size();
+        int j=0;
+        List<Integer>ans=new ArrayList<>(player.size());
+        for(int i=player.size()-1;i>=0;i--)
+        {
+            if(player.get(i)<ranked.get(num-1))
+            {
+                ans.add(num+1);
+            }
+            else{
+                for(;j<num;j++)
+                {
+                    if(player.get(i)>=ranked.get(j))
+                    {
+                        ans.add(j+1);
+                        break;
+                    }
+                }
+            }
+        }
+        ans.sort(Collections.reverseOrder());
+        return ans;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int rankedCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ranked = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int playerCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> player = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.climbingLeaderboard(ranked, player);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
