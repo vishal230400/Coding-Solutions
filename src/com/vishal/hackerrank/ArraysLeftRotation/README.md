6567bc1e1b778527c17c2ddbecf026d1933c67fd
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-29 19:03:04 +0530

    array left rotate

---
+++ src/com/vishal/hackerrank/ArraysLeftRotation/README.md
@@ -0,0 +1,2 @@
+# Question Name : ArraysLeftRotation
+Given an array a of n integers and a number, d, perform d left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
---
+++ src/com/vishal/hackerrank/ArraysLeftRotation/Solution.java
@@ -0,0 +1,67 @@
+package com.vishal.hackerrank.ArraysLeftRotation;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'rotLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER d
+     */
+
+
+    static void leftRotatebyOne(List<Integer> arr, int n)
+    {
+        int i, temp;
+        temp = arr.get(0);
+        for (i = 0; i < n - 1; i++)
+            arr.set(i,arr.get(i+1));
+        arr.set(n-1,temp);
+    }
+    public static List<Integer> rotLeft(List<Integer> a, int d) {
+        // Write your code here
+        for (int i = 0; i < d; i++)
+            leftRotatebyOne(a, a.size());
+
+        return a;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.rotLeft(a, d);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
