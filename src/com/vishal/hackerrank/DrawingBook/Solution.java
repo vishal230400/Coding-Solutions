fbe40cbbe6b055498e092b42c967197e0d846047
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-31 18:27:35 +0530

    drawing book

---
+++ src/com/vishal/hackerrank/DrawingBook/Solution.java
@@ -0,0 +1,40 @@
+package com.vishal.hackerrank.DrawingBook;
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p) {
+        // Write your code here
+        return Math.min(p/2,(n-p+(n-1)%2)/2);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int p = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
