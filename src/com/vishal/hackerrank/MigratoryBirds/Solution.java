24facab3a5679043e7eaf71049c44c9422eb9e20
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-31 18:40:42 +0530

    Migratory Birds

---
+++ src/com/vishal/hackerrank/MigratoryBirds/Solution.java
@@ -0,0 +1,59 @@
+package com.vishal.hackerrank.MigratoryBirds;
+
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'migratoryBirds' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int migratoryBirds(List<Integer> arr) {
+        // Write your code here
+        int []count=new int[5];
+        for(int i=0;i<5;i++)
+        {
+            count[i]=0;
+        }
+        for (Integer integer : arr) {
+            count[integer - 1]++;
+        }
+        int max=0;
+        for(int i=1;i<5;i++)
+        {
+            if(count[i]>count[max])
+                max=i;
+        }
+
+        return max+1;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
