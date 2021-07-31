9c6d67af3a1fcdfd58ec67e22f29a68863170138
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-07-31 18:22:37 +0530

    counting valley

---
+++ src/com/vishal/hackerrank/CountingValleys/Solution.java
@@ -0,0 +1,52 @@
+package com.vishal.hackerrank.CountingValleys;
+
+import java.io.*;
+
+class Result {
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+        int count=0,a=0,b=0;
+        for(int i=0;i<path.length();i++)
+        {
+            if(path.charAt(i)=='U')
+                a++;
+            else a--;
+            if(a==-1 && b==0)
+            {
+                count++;
+            }
+            b=a;
+        }
+        return count;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int steps = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String path = bufferedReader.readLine();
+
+        int result = Result.countingValleys(steps, path);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
