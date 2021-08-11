72b0c62e786bbb4ef94f48935f1828f76674aa10
vishal230400 <43965809+vishal230400@users.noreply.github.com>
2021-08-11 17:58:31 +0530

    spc

--- src/com/vishal/codechef/SPCTRIPS/Solution.java
+++ src/com/vishal/codechef/SPCTRIPS/Solution.java
@@ -6,8 +6,26 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n      = sc.nextInt();
-
+        int t      = sc.nextInt();
+        while (t-->0)
+        {
+            int n      = sc.nextInt();
+            int ans=0;
+            for(int c=1;c<=n;c++)
+            {
+                for(int b=c;b<=n;b+=c)
+                {
+                    for(int a=c;a<=n;a+=b)
+                    {
+                        if(a%b==c)
+                        {
+                            ans++;
+                        }
+                    }
+                }
+            }
+            System.out.println(ans);
+        }
         sc.close();
    }
 }
